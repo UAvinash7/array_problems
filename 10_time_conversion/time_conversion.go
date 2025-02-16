0a92a59a1c5bcbd543d45fb51f4490fe6bdeac09
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-02-16 19:21:09 +0530

    continuation

--- 10_time_conversion/time_conversion.go
+++ 10_time_conversion/time_conversion.go
@@ -48,3 +48,56 @@ Sample Output 0
 
 // Solution
 
+package main
+
+import (
+	"fmt"
+	"strings"
+	"strconv"
+)
+
+func timeConversion(s string) string {
+	//format of string input HH:MM:SSAM or HH:MM:SSPM i.e., Hour:Minute:SecondAM/PM
+
+	var findAMOrPM string = s[len(s)-2:]	// identifies whether it's AM or PM
+
+	// spliting the format of 12 hours numerical value into hours, minutes and seconds.
+	timeFormat := strings.Split(s[:len(s)-2], ":")
+	hh := timeFormat[0]
+	mm := timeFormat[1]
+	ss := timeFormat[2]
+
+	// converting the value of hour, minute and second variable from string into integer
+	hour, _ := strconv.Atoi(hh)
+	minute, _ := strconv.Atoi(mm)
+	second, _ := strconv.Atoi(ss)
+
+	if hour < 12 && findAMOrPM == "PM" {
+		hour += 12
+	}
+
+	if hour == 12 && findAMOrPM == "AM" {
+		hour = 0
+	}
+	
+	return fmt.Sprintf("%02d:%02d:%02d%s\n", hour, minute, second, findAMOrPM)
+	
+
+}
+
+func main() {
+	
+	// 12 hour format input
+	var str1 string = "07:05:45AM"
+	var str2 string = "07:05:45PM"
+	var str3 string = "12:05:59AM"
+	var str4 string = "13:41:54PM"
+ 
+	// we will get 24 hour military format output
+	fmt.Println(timeConversion(str1))
+	fmt.Println(timeConversion(str2))
+	fmt.Println(timeConversion(str3))
+	fmt.Println(timeConversion(str4))
+
+
+}
