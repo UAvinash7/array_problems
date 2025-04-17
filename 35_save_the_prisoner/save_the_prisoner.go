d711396c92830b5e79757acce3c2e5a279e9d0a4
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-04-17 22:10:11 +0530

    continuation

--- 35_save_the_prisoner/save_the_prisoner.go
+++ 35_save_the_prisoner/save_the_prisoner.go
@@ -8,10 +8,11 @@ The jailer is playing a little joke, though. The last piece of candy looks like 
 
 Example
 
+n = 4
+m = 6
+s = 2
 
-
-
-There are  prisoners,  pieces of candy and distribution starts at chair . The prisoners arrange themselves in seats numbered  to . Prisoners receive candy at positions . The prisoner to be warned sits in chair number .
+There are 4 prisoners, 6 pieces of candy and distribution starts at chair 2. The prisoners arrange themselves in seats numbered 1 to 4. Prisoners receive candy at positions 2, 3, 4, 1, 2, 3. The prisoner to be warned sits in chair number 3.
 
 Function Description
 
@@ -22,48 +23,60 @@ saveThePrisoner has the following parameter(s):
 int n: the number of prisoners
 int m: the number of sweets
 int s: the chair number to begin passing out sweets from
+
 Returns
 
 int: the chair number of the prisoner to warn
+
 Input Format
 
-The first line contains an integer, , the number of test cases.
-The next  lines each contain  space-separated integers:
+The first line contains an integer, t, the number of test cases.
+The next t lines each contain 3 space-separated integers:
 
-: the number of prisoners
-: the number of sweets
-: the chair number to start passing out treats at
+n : the number of prisoners
+m : the number of sweets
+s : the chair number to start passing out treats at
+
 Constraints
+
+1 <= t <= 100
+1 <= n <= 10 ^ 9
+1 <= m <= 10 ^ 9
+1 <= s <= n
 
 Sample Input 0
 
 2
 5 2 1
 5 2 2
+
 Sample Output 0
 
 2
 3
+
 Explanation 0
 
-In the first query, there are  prisoners and  sweets. Distribution starts at seat number . Prisoners in seats numbered  and  get sweets. Warn prisoner .
+In the first query, there are n = 5 prisoners and m = 2 sweets. Distribution starts at seat number s = 1. Prisoners in seats numbered 1 and 2 get sweets. Warn prisoner 2.
 
-In the second query, distribution starts at seat  so prisoners in seats  and  get sweets. Warn prisoner .
+In the second query, distribution starts at seat 2 so prisoners in seats 2 and 3 get sweets. Warn prisoner 3.
 
 Sample Input 1
 
 2
 7 19 2
 3 7 3
+
 Sample Output 1
 
 6
 3
+
 Explanation 1
 
-In the first test case, there are  prisoners,  sweets and they are passed out starting at chair . The candies go all around twice and there are  more candies passed to each prisoner from seat  to seat .
+In the first test case, there are n = 7 prisoners, m = 19 sweets and they are passed out starting at chair s = 2. The candies go all around twice and there are 5 more candies passed to each prisoner from seat 2 to seat 6.
 
-In the second test case, there are  prisoners,  candies and they are passed out starting at seat . They go around twice, and there is one more to pass out to the prisoner at seat .
+In the second test case, there are n = 3 prisoners, m = 7 candies and they are passed out starting at seat s = 3. They go around twice, and there is one more to pass out to the prisoner at seat 3.
 
 */
 
