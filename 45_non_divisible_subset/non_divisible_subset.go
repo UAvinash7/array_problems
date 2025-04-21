8ff4468b6c26b049114706dda4b316d9b045b252
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-04-21 22:30:03 +0530

    continuation

---
+++ 45_non_divisible_subset/non_divisible_subset.go
@@ -0,0 +1,57 @@
+/*
+
+Problem Statement
+
+Given a set of distinct integers, print the size of a maximal subset of s where the sum of any 2 numbers in s' is not evenly divisible by k.
+
+Example
+
+s = [19, 10, 12, 10, 24, 25, 22]
+k = 4
+
+One of the arrays that can be created is s'[0] = [10, 12, 25]. Another is s'[1] = [19, 22, 24]. After testing all permutations, the maximum length solution array has  elements.
+
+Function Description
+
+Complete the nonDivisibleSubset function in the editor below.
+
+nonDivisibleSubset has the following parameter(s):
+
+int S[n]: an array of integers
+int k: the divisor
+Returns
+
+int: the length of the longest subset of  meeting the criteria
+Input Format
+
+The first line contains  space-separated integers,  and , the number of values in  and the non factor.
+The second line contains  space-separated integers, each an , the unique values of the set.
+
+Constraints
+
+All of the given numbers are distinct.
+Sample Input
+
+STDIN    Function
+-----    --------
+4 3      S[] size n = 4, k = 3
+1 7 2 4  S = [1, 7, 2, 4]
+Sample Output
+
+3
+Explanation
+
+The sums of all permutations of two elements from  are:
+
+1 + 7 = 8
+1 + 2 = 3
+1 + 4 = 5
+7 + 2 = 9
+7 + 4 = 11
+2 + 4 = 6
+Only  will not ever sum to a multiple of .
+
+*/
+
+//Solution
+
