79badd5a8fb37eb23a57bc5da443948ed970a7ea
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-05-28 21:40:18 +0530

    continuation

--- 13_number_line_jumps/number_lines_jumps.go
+++ 13_number_line_jumps/number_lines_jumps.go
@@ -66,10 +66,15 @@ The second kangaroo has a starting location that is ahead (further to the right)
 
 package main
 
+import "fmt"
+
 func kangaroo(x1 int32,  v1 int32, x2 int32, v2 int32) string {
 
 }
 
 func main() {
-	
+	var x1, v1, x2, v2 int32
+	fmt.Printf("Enter the value of x1: ")
+	fmt.Scanf("%d\n", &x1)
+	fmt.Print("Enter the value")
 }
