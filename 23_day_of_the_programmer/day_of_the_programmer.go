382eafc0d42fa8dd83edbc6b0c167f89e824abf9
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-10-26 22:15:47 +0530

    continuation

--- 23_day_of_the_programmer/day_of_the_programmer.go
+++ 23_day_of_the_programmer/day_of_the_programmer.go
@@ -75,3 +75,27 @@ Since 1800 is leap year as per Julian calendar. Day lies on 12 September.
 
 // Solution
 
+package main
+
+import "fmt"
+
+func main() {
+	var year int
+	fmt.Scan(&year)
+
+	if year < 1918 {
+		if year%4 == 0 {
+			fmt.Printf("12.09.%v", year)
+		} else {
+			fmt.Printf("13.09.%v", year)
+		}
+	} else if year > 1918 {
+		if year%400 == 0 || (year%4 == 0 && year%100 != 0) {
+			fmt.Printf("12.09.%v", year)
+		} else {
+			fmt.Printf("13.09.%v", year)
+		}
+	} else {
+		fmt.Printf("26.09.1918")
+	}
+}
