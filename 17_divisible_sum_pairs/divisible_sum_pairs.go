637caa3e240a4f16caf291b8818f71780ad4af4b
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-11-09 22:22:24 +0530

    continuation

--- 17_divisible_sum_pairs/divisible_sum_pairs.go
+++ 17_divisible_sum_pairs/divisible_sum_pairs.go
@@ -59,7 +59,75 @@ Here are the 5 valid pairs when k = 3:
 
 package main
 
-func main() {
-	
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// divisibleSumPairs finds the number of pairs (i, j) such that i < j and ar[i] + ar[j] is divisible by k.
+func divisibleSumPairs(n int32, k int32, ar []int32) int32 {
+	var count int32 = 0
+	for i := 0; i < int(n); i++ {
+		for j := i + 1; j < int(n); j++ {
+			if (ar[i]+ar[j])%k == 0 {
+				count++
+			}
+		}
+	}
+	return count
 }
- 
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	nTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	kTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+	checkError(err)
+	k := int32(kTemp)
+
+	arTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var ar []int32
+	for i := 0; i < int(n); i++ {
+		arItemTemp, err := strconv.ParseInt(arTemp[i], 10, 64)
+		checkError(err)
+		arItem := int32(arItemTemp)
+		ar = append(ar, arItem)
+	}
+
+	result := divisibleSumPairs(n, k, ar)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
