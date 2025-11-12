0440ad8206ba9bf88c12ab1069332c517b24d8d2
avinash upadhyayula <avinash.upadhyayula@gmail.com>
2025-11-12 22:56:00 +0530

    continuation

--- 50_acm_icpc_team/acm_icpc_team.go
+++ 50_acm_icpc_team/acm_icpc_team.go
@@ -78,3 +78,44 @@ The 2 teams (1, 3) and (3, 4) know all 5 topics which is maximal.
 
 // Solution
 
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n, m int
+
+	fmt.Scan(&n, &m)
+
+	p := make([]string, n)
+
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%s", &p[i])
+	}
+
+	topics := 0
+	teams := 0
+	one := uint8('1')
+
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			ct := 0
+			for k := 0; k < m; k++ {
+				if p[i][k] == one || p[j][k] == one {
+					ct++
+				}
+			}
+
+			if ct > topics {
+				topics = ct
+				teams = 1
+			} else if ct > 0 && topics == ct {
+				teams++
+			}
+		}
+	}
+
+	fmt.Printf("%v\n%v\n", topics, teams)
+}
